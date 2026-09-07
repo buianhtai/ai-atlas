6cd37fe9785112b34aefe1200c95859672d938c9
Tai.Bui <buianhtai90@gmail.com>
2026-09-07 16:43:41 +0700

    feat: load catalog family styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./editorial.css";
 import "./home-v2.css";
 import "./lesson.css";
 import "./shell.css";
+import "./catalog-v3.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
